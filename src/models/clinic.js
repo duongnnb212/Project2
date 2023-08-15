715c5ad1dd4d5285e87ec0710a1476446b917253
NguyenNgocBinhDuong <duong.nnb204734@sis.hust.edu.vn>
2023-08-15 09:13:26 +0700

    second commit

--- src/models/clinic.js
+++ src/models/clinic.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Clinic extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,17 +13,14 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  User.init({
-    id: DataTypes.INTEGER,
-    email: DataTypes.STRING,
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
+  Clinic.init({
     address: DataTypes.STRING,
+    description: DataTypes.STRING,
     gender: DataTypes.BOOLEAN,
     roleid: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Clinic',
   });
-  return User;
+  return Clinic;
 };
